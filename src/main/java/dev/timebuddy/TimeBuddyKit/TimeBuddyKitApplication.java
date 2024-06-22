6cec506d552ca492db00a6c2d9f74869e3c8c9b2
Nils Zentgraf <nils.zentgraf@aol.com>
2024-06-22 10:31:19 +0200

    init

---
+++ src/main/java/dev/timebuddy/TimeBuddyKit/TimeBuddyKitApplication.java
@@ -0,0 +1,13 @@
+package dev.timebuddy.TimeBuddyKit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBuddyKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeBuddyKitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/timebuddy/TimeBuddyKit/TimeBuddyKitApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.timebuddy.TimeBuddyKit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeBuddyKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
